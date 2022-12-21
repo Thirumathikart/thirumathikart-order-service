96b08e37f269fbac3298aecc811c301d9c6895b9
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-12-21 17:16:41 +0530

    feat(rpcs): Add User RPC

--- models/fetch_order.go
+++ models/fetch_order.go
@@ -1,8 +1,13 @@
 package models
 
-import "github.com/thirumathikart/thirumathikart-order-service/schemas"
+import (
+	"github.com/thirumathikart/thirumathikart-order-service/generated/user"
+	"github.com/thirumathikart/thirumathikart-order-service/schemas"
+)
 
 type FetchOrder struct {
-	Order       schemas.Order 			`json:"order"`
-	OrderItem   []schemas.OrderItem 	`json:"items"`
+	Order           schemas.Order       `json:"order"`
+	OrderItem       []schemas.OrderItem `json:"items"`
+	CustomerAddress *user.Address       `json:"customer_address"`
+	SellerAddress   *user.Address       `json:"seller_address"`
 }
--- services/fetch_order.go
+++ services/fetch_order.go
@@ -1,19 +1,21 @@
 package services
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/thirumathikart/thirumathikart-order-service/config"
 	"github.com/thirumathikart/thirumathikart-order-service/generated/user"
+	"github.com/thirumathikart/thirumathikart-order-service/helpers"
 	"github.com/thirumathikart/thirumathikart-order-service/middlewares"
 	"github.com/thirumathikart/thirumathikart-order-service/models"
 )
 
-
-func (os *orderService) FetchOrderBySeller (c echo.Context,
+func (os *orderService) FetchOrderBySeller(c echo.Context,
 	userDetails *user.User) error {
-	
-	orders, err :=os.repo.FetchOrderBySeller(uint(userDetails.UserId))
+
+	orders, err := os.repo.FetchOrderBySeller(uint(userDetails.UserId))
 
 	if err != nil {
 		return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
@@ -26,18 +28,31 @@ func (os *orderService) FetchOrderBySeller (c echo.Context,
 		if err != nil {
 			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
 		}
-		response = append(response, models.FetchOrder{Order: order,OrderItem: items})
+		customerRes, err := helpers.GRPCDialler(config.AuthService, "user", order.CustomerID)
+		if err != nil {
+			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
+		}
+		sellerRes, err := helpers.GRPCDialler(config.AuthService, "user", order.SellerID)
+		if err != nil {
+			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
+		}
+		fmt.Print((customerRes.(*user.UserResponse)).User, (sellerRes.(*user.UserResponse)).User)
+		if err != nil {
+			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
+		}
+		CustomerAddress := customerRes.(*user.UserResponse).User.Address
+		SellerAddress := sellerRes.(*user.UserResponse).User.Address
+		response = append(response, models.FetchOrder{Order: order, OrderItem: items, CustomerAddress: CustomerAddress, SellerAddress: SellerAddress})
 	}
 
-
 	//TODO: fetch address and user details if required
-	return middlewares.Responder(c, http.StatusOK,  response)
+	return middlewares.Responder(c, http.StatusOK, response)
 }
 
-func (os *orderService) FetchOrderByDeliveryPartner (c echo.Context,
+func (os *orderService) FetchOrderByDeliveryPartner(c echo.Context,
 	userDetails *user.User) error {
-	
-	orders, err :=os.repo.FetchOrderByDeliveryPartner(uint(userDetails.UserId))
+
+	orders, err := os.repo.FetchOrderByDeliveryPartner(uint(userDetails.UserId))
 
 	if err != nil {
 		return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
@@ -49,33 +64,31 @@ func (os *orderService) FetchOrderByDeliveryPartner (c echo.Context,
 		if err != nil {
 			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
 		}
-		response = append(response, models.FetchOrder{Order: order,OrderItem: items})
+		response = append(response, models.FetchOrder{Order: order, OrderItem: items})
 	}
 
-
 	//TODO: fetch address and user details if required
-	return middlewares.Responder(c, http.StatusOK,  response)
+	return middlewares.Responder(c, http.StatusOK, response)
 }
 
-func (os *orderService) FetchOrderByCustomer (c echo.Context,
+func (os *orderService) FetchOrderByCustomer(c echo.Context,
 	userDetails *user.User) error {
-	
-	orders, err :=os.repo.FetchOrderByCustomer(uint(userDetails.UserId))
+
+	orders, err := os.repo.FetchOrderByCustomer(uint(userDetails.UserId))
 
 	if err != nil {
 		return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
 	}
 	var response []models.FetchOrder
 
-	for _, order := range orders{
+	for _, order := range orders {
 		items, err := os.repo.FetchOrderItemsByOrder(order.ID)
 		if err != nil {
 			return middlewares.Responder(c, http.StatusNoContent, "Unable to Fetch Order")
 		}
-		response = append(response, models.FetchOrder{Order: order,OrderItem: items})
+		response = append(response, models.FetchOrder{Order: order, OrderItem: items})
 	}
 
 	//TODO: fetch address and user details if required
-	return middlewares.Responder(c, http.StatusOK,  response)
+	return middlewares.Responder(c, http.StatusOK, response)
 }
-
